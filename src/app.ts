12d0751026a9d9900f0d63e11798c6e0bab3f219
defycab <love.bellinder@gmail.com>
2024-11-02 14:49:10 +0100

    creating a virtual db for testing routes

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,27 @@
 import express from "express";
 import { createStudentsFeature } from "./features";
 import cors from "cors";
+import { v4 as uuidv4 } from "uuid";
+
+const createDB = () => {
+  const Students = [
+    {
+      id: uuidv4(),
+      name: "Love",
+    },
+  ];
+
+  return {
+    getAll: async () => Students,
+  };
+};
+
+const db = createDB();
 
 export const createApp = () => {
   const app = express();
 
   app.use(express.json());
-
   app.use(cors());
 
   const studentsFeature = createStudentsFeature();
@@ -14,7 +29,7 @@ export const createApp = () => {
   app.get("/", (req, res) => {
     res.json([]);
   });
-  
+
   app.use("/api/v1/students", studentsFeature.getRouter());
 
   return app;
