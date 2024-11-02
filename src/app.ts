bc4605ce376944d0ecc3b1b6293e91e0752f07c4
defycab <love.bellinder@gmail.com>
2024-11-02 13:38:23 +0100

    getting an empty array from studentfeature

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,25 @@
 import express from "express";
+import { createStudentsFeature } from "./features";
+import cors from "cors";
 
 export const createApp = () => {
   const app = express();
 
   app.use(express.json());
 
+  app.use(cors());
+
   app.get("/status", (req, res) => {
     res.json({ status: "ready" });
   });
+
+  const studentsFeature = createStudentsFeature();
+
+  app.get("/", (req, res) => {
+    res.json([]);
+  });
+  
+  app.use("/api/v1/students", studentsFeature.getRouter());
 
   return app;
 };
