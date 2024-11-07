e7ebb3887aa6c8fd1a287b018d06d3f7eba9dec4
defycab <love.bellinder@gmail.com>
2024-11-07 22:25:07 +0100

    minor fixing

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from "drizzle-kit";
 export default defineConfig({
   // pattern features
   schema: "./src/drizzle/schema.ts",
-  // "./src/features/**/db-schema.ts"
   out: "./src/drizzle/migrations",
   dialect: "postgresql",
   dbCredentials: {
