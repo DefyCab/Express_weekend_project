4bcb3b37dcbec2a14241b83660ea6b5f0f4b491b
defycab <love.bellinder@gmail.com>
2024-11-10 21:42:21 +0100

    creating schema after josn fixture model

--- src/drizzle/schema.ts
+++ src/drizzle/schema.ts
@@ -1,8 +1,14 @@
-import { integer, pgTable, uuid, varchar } from "drizzle-orm/pg-core";
+import { integer, pgTable, uuid, varchar, jsonb } from "drizzle-orm/pg-core";
 
 export const StudentsTable = pgTable("students", {
   id: uuid("id").primaryKey().defaultRandom(),
   name: varchar("name", { length: 255 }).notNull(),
   email: varchar("email", { length: 255 }).notNull().unique(),
   age: integer("age").notNull(),
+});
+
+export const CoursesTable = pgTable("courses", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  studentId: uuid("studentId").notNull(),
+  courses: jsonb("courses").notNull(),
 });
